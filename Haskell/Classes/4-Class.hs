3304abccf7cac511d6b7662cb006b6874e616ce1
Shimanogov Igor <shimanogov.in@phystech.edu>
2025-04-16 14:27:52 +0300

    added contents of 4 hs class

--- Haskell/Classes/4-Class.hs
+++ Haskell/Classes/4-Class.hs
@@ -1,61 +1,65 @@
+{-# LANGUAGE MonadComprehensions #-}
 import Control.Monad (ap, liftM, MonadPlus)
-import GHC.Base (Alternative(..))
-import Data.Char (ord)
-
+import Control.Applicative (Alternative (..))
+import Data.Char (digitToInt)
+import GHC.Base (ord)
 -- type Parser = String -> Tree
--- type Parser = String -> (Tree,String)
--- type Parser = String -> [(Tree,String)]
--- type Parser a = String -> [(a,String)]
+-- type Parser = String -> (Tree, String)
+-- type Parser = String -> [(Tree, String)]
+-- type Parser a = String -> [(a, String)]
 -- type Parser s a = s -> [(a, s)]
--- newtype Parser a = Parser {runParse :: String -> [(a,String)]}
+-- type Parser a = String -> Maybe (a, String)
 newtype Parser a = Parser {runParse :: String -> Maybe (a, String)}
 
 pureP :: a -> Parser a
---pureP v = \inp -> [(v, inp)]
-pureP v =  Parser $ \inp -> Just (v, inp)
+pureP v = Parser $ \inp -> Just (v, inp)
 zero :: Parser a
-zero  = Parser $ const Nothing
+zero = Parser $ const Nothing
 item :: Parser Char
 item = Parser $ \inp -> case inp of
     [] -> Nothing
-    (x:xs) -> Just (x,xs)
+    (x:xs) -> Just (x, xs)
 
 bind :: Parser a -> (a -> Parser b) -> Parser b
 p `bind` f = Parser $ \inp -> case runParse p inp of
     Nothing -> Nothing
     Just (v, inp') -> runParse (f v) inp'
 
--- Отсюда уже видно, что Parser -- это монада!
+-- >>= !
+
 instance Functor Parser where
     fmap :: (a -> b) -> Parser a -> Parser b
     fmap = liftM
+
 instance Applicative Parser where
     pure :: a -> Parser a
     pure = pureP
     (<*>) :: Parser (a -> b) -> Parser a -> Parser b
     (<*>) = ap
+
 instance Monad Parser where
     (>>=) :: Parser a -> (a -> Parser b) -> Parser b
     (>>=) = bind
+
 instance Alternative Parser where
     empty :: Parser a
     empty = zero
     (<|>) :: Parser a -> Parser a -> Parser a
-    Parser l <|> Parser r = Parser $ \input ->
-        case l input of
-        Nothing ->
-            case r input of
-            Nothing -> Nothing
-            rs -> rs
-        ls -> ls
+    Parser l <|> Parser r = Parser $ \inp ->
+        case l inp of
+            Nothing -> 
+                case r inp of
+                    Nothing -> Nothing
+                    rs -> rs
+            ls -> ls
+        
 instance MonadPlus Parser
 
 sat :: (Char -> Bool) -> Parser Char
-sat f = item >>= \x ->
-    if f x then pure x else zero
+sat f = item >>= (\x -> if f x then pure x else zero)
 
 char :: Char -> Parser Char
-char c = sat (== c)
+char c = sat (==c)
 
 lower :: Parser Char
 lower = sat (`elem` ['a'..'z'])
@@ -69,18 +73,18 @@ alpha = lower <|> upper
 num :: Parser Char
 num = sat (`elem` ['0'..'9'])
 
-alphanum :: Parser Char
+alphanum :: Parser Char 
 alphanum = alpha <|> num
-
-satstring :: [Char -> Bool] -> Parser String
-satstring [] = pure ""
-satstring (x:xs) = sat x >>= \y -> satstring xs >>= \ys -> pure (y:ys)
 
 satstring' :: [Char -> Bool] -> Parser String
 satstring' [] = pure ""
-satstring' (x:xs) = do
+satstring' (x:xs) = sat x >>= (\y -> satstring' xs >>= \ys -> pure (y:ys))
+
+satstring :: [Char -> Bool] -> Parser String
+satstring [] = pure ""
+satstring (x : xs)= do 
     y <- sat x
-    ys <- satstring' xs
+    ys <- satstring xs
     return (y:ys)
 
 word :: String -> Parser String
@@ -88,22 +92,18 @@ word s = satstring $ map (==) s
 
 star1 :: Parser a -> Parser [a]
 star1 p = do
-    y <- p 
-    ys <- star p <|> pure []
+    y <- p
+    ys <- star1 p <|> pure []
     return $ y : ys
 
 star :: Parser a -> Parser [a]
 star p = star1 p <|> pure []
 
 try :: Parser a -> Parser ()
-try p = p *> pure ()
-
-ifPossible :: Parser String -> Parser String
-ifPossible p = p <|> pure ""
-
--- Вспомним про операции в PEG.
-
--- Ладно, давайте напишем что-нибудь. Хотя бы тот же парсер арифметических выражений
+try p = Parser $ \inp ->
+    case runParse p inp of
+        Nothing -> Nothing
+        _ -> Just ((), inp)
 
 data Expression =
     Var |
@@ -113,20 +113,20 @@ data Expression =
     Sin Expression |
     Cos Expression |
     Pow Expression Int
-    deriving (Show, Eq)
+    deriving (Eq, Show)
 
 varParser :: Parser Expression
 varParser = do
     char 'x'
     return Var
 
-digitToInt :: Char -> Int
-digitToInt x = ord x - ord '0'
+dgToInt :: Char -> Int
+dgToInt x = ord x - ord '0' 
 
 natParser :: Parser Int
 natParser = do
     n <- star1 num
-    let ns = map digitToInt n
+    let ns = map dgToInt n
     return $ foldl (\x y -> x * 10 + y) 0 ns
 
 intParser :: Parser Int
@@ -151,7 +151,7 @@ mulParser = do
     char '*'
     y <- exprParser
     char ')'
-    return $ Sum x y
+    return $ Mult x y
 
 sinParser :: Parser Expression
 sinParser = fmap Sin $ word "sin(" *> exprParser <* char ')'
@@ -160,13 +160,41 @@ cosParser :: Parser Expression
 cosParser = fmap Cos $ word "cos(" *> exprParser <* char ')'
 
 powParser :: Parser Expression
-powParser = do
+powParser = do 
     char '('
     x <- exprParser
     char '^'
-    y <- intParser
+    i <- intParser
     char ')'
-    return $ Pow x y
+    return $ Pow x i
 
 exprParser :: Parser Expression
-exprParser = varParser <|> constParser <|> sumParser <|> mulParser <|> sinParser <|> cosParser <|> powParser
+exprParser = varParser <|> constParser <|> sumParser <|> mulParser <|>
+    sinParser <|> cosParser <|> powParser
+
+ex :: String
+ex = "sin(((3+(x^5))*6))"
+-- Just (Sin (Mult (Sum (Const 3) (Pow Var 5)) (Const 6)),"")
+
+listComp :: [Integer]
+listComp = [x+y | x <- [1..10], y <- [10..15]]
+
+listComp' :: [Integer]
+listComp' = do
+    x <- [1..10]
+    y <- [10..15]
+    return $ x + y
+
+mulParser' :: Parser Expression
+mulParser' = do
+    char '('
+    x <- exprParser
+    char '*'
+    y <- exprParser
+    char ')'
+    return $ Mult x y
+
+mulParser'' :: Parser Expression
+mulParser'' = [Mult x y | 
+    x <- char '(' *> exprParser <* char '*',
+    y <- exprParser <* char ')']
