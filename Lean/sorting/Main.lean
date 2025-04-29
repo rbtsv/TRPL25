bd84e9882c08502a40027ec6b16b18a553121c6f
Lean 4 VS Code Extension <>
2025-04-29 17:34:56 +0300

    Initial commit

---
+++ Lean/sorting/Main.lean
@@ -0,0 +1,4 @@
+import Sorting
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ Lean/sorting/Sorting.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Sorting` library.
+-- Import modules here that should be built as part of the library.
+import Sorting.Basic
